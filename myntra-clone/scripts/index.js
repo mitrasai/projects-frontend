4167a5f7e4e18e9f987f42b74e480c59e333eae0
Sai-Mithra <105640472+mitrasai@users.noreply.github.com>
2024-08-07 20:05:20 +0530

    Add files via upload

---
+++ myntra-clone/scripts/index.js
@@ -0,0 +1,51 @@
+let bagItems;
+onLoad();
+
+function onLoad() {
+  let itemsInBag = localStorage.getItem('bagItems');
+  bagItems = itemsInBag ? JSON.parse(itemsInBag) : [];
+  displayItems();
+  displayBagIcon();
+}
+
+function addToBag(itemId){
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCount = document.querySelector('.count-bag-item');
+  if(bagItems.length > 0) { 
+    bagItemCount.innerText = bagItems.length;
+    bagItemCount.style.visibility = 'visible';
+  }
+  else bagItemCount.style.visibility = 'hidden';
+}
+
+function displayItems() {
+  let itemsContainer = document.querySelector('.items-container');
+  let innerHTML = '';
+
+  if(!itemsContainer) return;
+
+  items.forEach(item => {
+    innerHTML += 
+    `<div class="item-container">
+    <img class="item-img" src="${item.item_image}" alt="ear-rings-item_image">
+    <div class="rating">
+      ${item.rating.stars} ⭐ | ${item.rating.noOfReviews}
+    </div>
+    <div class="brand">${item.company_name}</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+      <span class="current-price">Rs ${item.current_price}</span>
+      <span class="original-price">Rs ${item.original_price}</span>
+      <span class="disnoOfReviews">(${item.discount_percentage}% OFF)</span>
+    </div>
+    <button class="bag-btn" onclick="addToBag(${item.id})">Add to Bag</button>
+  </div>`
+  });
+
+  itemsContainer.innerHTML = innerHTML;
+}
